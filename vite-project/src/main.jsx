5cc06c6bd7311a1d8dbc53027a16904aef2e8e46
axs-seahorse7 <axs.sh7@gmail.com>
2025-03-23 17:05:22 +0530

    update userContext from UserContext

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
-import { UserProvider } from '../Global/UserContext'
+import { UserProvider } from '../Global/userContext.jsx'
 
 import './index.css'
 import App from './App.jsx'
