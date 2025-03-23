202c9598a83fcdb2fe786de9c467ee0675545101
bfsi-seekersfinder <finderseekers0@gmail.com>
2025-03-23 20:58:57 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -26,7 +26,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 const corsOptions = {
-    origin: ["https://talentx.onrender.com"],
+    origin: "https://talentx.onrender.com",
     methods: ["GET","POST","PUT","DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials:true 
