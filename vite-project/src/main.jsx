9d99b418360def0685fcb177933b1bb55729e81c
axs-seahorse7 <axs.sh7@gmail.com>
2025-03-23 17:01:27 +0530

    upper-case testing UserContext

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
-import { UserProvider } from '../Global/userContext'
+import { UserProvider } from '../Global/UserContext'
+
 import './index.css'
 import App from './App.jsx'
 
